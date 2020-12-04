c0594556c38d4cb89006a57bb350290ea0371776
Dastamn <mezhoudi.nazim@gmail.com>
2020-12-04 22:42:17 +0100

    Added background image support, removed recursive calls

--- src/index.js
+++ src/index.js
@@ -6,28 +6,27 @@ const cheerio = require("cheerio");
 
 const SAVE_DIR = "./images";
 
-const isBase64 = src => src.search(/^data:image\/(jpe?g|png);base64, ?/g) == 0;
+const IMG_VAILD_FORMAT = /.(jpe?g|png)$/i;
+const IMG_URL = /(http(s?):)([\/.\w\s-])*\.(?:jpe?g|png)/g;
 
-const isValidFormat = src => src.search(/.(jpe?g|png)$/i) > 0;
+const isValidFormat = src => src.search(IMG_VAILD_FORMAT) > 0;
 
 const completeUrl = ({ protocol, host, path }) =>
   path.startsWith("http")
     ? path
     : `${protocol}//${host}${path.startsWith("/") ? "" : "/"}${path}`;
 
-const retrieveLinks = (aTags, { protocol, host }) =>
-  aTags
-    .map((_, a) => a.attribs.href)
-    .filter((_, link) => link && link.matches(/[#\/]/i))
-    .map((_, link) => completeUrl({ protocol, host, path: link }))
-    .filter((_, link) => link.startsWith(`${protocol}//${host}`))
-    .get();
-
 const retrieveImageUrls = (imgTags, { protocol, host }) =>
   imgTags
     .map((_, img) => img.attribs.src)
-    .filter((_, src) => !isBase64(src) && isValidFormat(src))
+    .filter((_, src) => isValidFormat(src))
     .map((_, src) => completeUrl({ protocol, host, path: src }))
+    .get();
+
+const retrieveBackgroundUrls = tags =>
+  tags
+    .map((_, tag) => tag.attribs.style)
+    .map((_, style) => style.match(IMG_URL))
     .get();
 
 const downloadImageFromUrl = url => {
@@ -40,25 +39,23 @@ const downloadImageFromUrl = url => {
     .catch(_ => console.error(`couldn't download image from ${url}`));
 };
 
-const crawl = async ({ url, seen = {} }) => {
-  if (!url || seen[url]) return;
-  seen[url] = true;
+const crawl = async ({ url, tags = [] }) => {
   const { host, protocol } = urlParser.parse(url);
   console.info(`crawling ${url}...`);
   const html = await fetch(url).then(res => res.text());
   const $ = cheerio.load(html);
   const $img = $("img");
-  const $a = $("a");
-  const imageUrls = retrieveImageUrls($img, { protocol, host });
-  const links = retrieveLinks($a, { protocol, host });
-  await Promise.all(
-    imageUrls.map(imageUrl => downloadImageFromUrl(imageUrl))
-  ).catch(err => console.log(err));
-  await Promise.all(links.map(link => crawl({ url: link, seen })));
+  const $tags = tags.map(tag => $(tag));
+  const imageUrls = [
+    ...retrieveImageUrls($img, { protocol, host }),
+    ...$tags.map($t => retrieveBackgroundUrls($t)).flat(1),
+  ];
+  await Promise.all(imageUrls.map(imageUrl => downloadImageFromUrl(imageUrl)));
 };
 
 if (!fs.existsSync(SAVE_DIR)) fs.mkdirSync(SAVE_DIR);
 
 crawl({
-  url: "https://anilist.co",
+  url: "",
+  tags: ["a", "div"],
 });
